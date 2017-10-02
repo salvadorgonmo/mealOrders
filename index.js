67c4bd280b81c42164c9c667fa4ca4469a648206
Salvador Gonzalez <salvador.gonzalez@alli-lt-170922a.ad.unosquare.com>
2017-10-02 12:33:33 -0500

    adding first version of office controller with errorHandlers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const port = 3030
 const mongoose = require('mongoose')
 const promise = require('bluebird')
+const errorHandlers = require('./utils/errorHandlers')
 mongoose.Promise = promise
 mongoose.connect('mongodb://localhost/foodControl', {
   useMongoClient: true
@@ -19,3 +20,15 @@ app.use('/', routes)
 app.listen(port, function () {
   console.log('app working on port:', port)
 })
+
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound)
+app.use(errorHandlers.catchErrors)
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors)
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors)
--- routes.js
+++ routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const Router = express.Router()
-
+const { catchErrors } = require('./utils/errorHandlers')
 const officeController = require('./controllers/office')
-Router.post('/office', officeController.post)
-Router.get('/office', officeController.get)
-Router.put('/office/:id', officeController.put)
-Router.delete('/office/:id', officeController.delete)
-Router.get('/office/:id', officeController.getOne)
+
+Router.post('/office', catchErrors(officeController.post))
+Router.get('/office', catchErrors(officeController.get))
+Router.put('/office/:id', catchErrors(officeController.put))
+Router.delete('/office/:id', catchErrors(officeController.delete))
+Router.get('/office/:id', catchErrors(officeController.getOne))
 
 module.exports = Router
