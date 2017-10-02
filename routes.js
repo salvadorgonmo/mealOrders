20bb80f3ae1c9502d87b81f0a737a0fd6e35524e
Salvador Gonzalez <salvador.gonzalez@alli-lt-170922a.ad.unosquare.com>
2017-10-02 14:50:41 -0500

    adding the modified routes.js file for all of the controllers

--- routes.js
+++ routes.js
@@ -3,6 +3,8 @@ const Router = express.Router()
 const { catchErrors } = require('./utils/errorHandlers')
 const officeController = require('./controllers/office')
 const listController = require('./controllers/list')
+const orderController = require('./controllers/order')
+const userController =  require('./controllers/user')
 
 Router.post('/office', catchErrors(officeController.post))
 Router.get('/office', catchErrors(officeController.get))
@@ -15,5 +17,17 @@ Router.get('/list', catchErrors(listController.get))
 Router.put('/list/:id', catchErrors(listController.put))
 Router.delete('/list/:id', catchErrors(listController.delete))
 Router.get('/list/:id', catchErrors(listController.getOne))
+
+Router.post('/order', catchErrors(orderController.post))
+Router.get('/order', catchErrors(orderController.get))
+Router.put('/order/:id', catchErrors(orderController.put))
+Router.delete('/order/:id', catchErrors(orderController.delete))
+Router.get('/order/:id', catchErrors(orderController.getOne))
+
+Router.post('/user', catchErrors(userController.post))
+Router.get('/user', catchErrors(userController.get))
+Router.put('/user/:id', catchErrors(userController.put))
+Router.delete('/user/:id', catchErrors(userController.delete))
+Router.get('/user/:id', catchErrors(userController.getOne))
 
 module.exports = Router
