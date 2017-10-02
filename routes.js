d270978d1bd06bd0e4c9add4fe7ac6c9d6e40fa5
Salvador Gonzalez <salvador.gonzalez@alli-lt-170922a.ad.unosquare.com>
2017-10-02 13:01:21 -0500

    adding controller for the model list.js with his error handlers

--- routes.js
+++ routes.js
@@ -2,11 +2,18 @@ const express = require('express')
 const Router = express.Router()
 const { catchErrors } = require('./utils/errorHandlers')
 const officeController = require('./controllers/office')
+const listController = require('./controllers/list')
 
 Router.post('/office', catchErrors(officeController.post))
 Router.get('/office', catchErrors(officeController.get))
 Router.put('/office/:id', catchErrors(officeController.put))
 Router.delete('/office/:id', catchErrors(officeController.delete))
 Router.get('/office/:id', catchErrors(officeController.getOne))
+
+Router.post('/list', catchErrors(listController.post))
+Router.get('/list', catchErrors(listController.get))
+Router.put('/list/:id', catchErrors(listController.put))
+Router.delete('/list/:id', catchErrors(listController.delete))
+Router.get('/list/:id', catchErrors(listController.getOne))
 
 module.exports = Router
